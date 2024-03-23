4ad4dbf58141415bdcf7dd85f169fb6d79dc211e
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-03-23 10:25:09 +0000

    Add colors, fonts, and other properties to Tailwind config file + Setup the HTML structure and integrate it + Start styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '24px',
+      }
+    },
+    colors: {
+      "white": "hsl(0, 0%, 100%)",
+      "light-pink": "hsl(275, 100%, 97%)",
+      "grayish-purple": "hsl(292, 16%, 49%)",
+      "dark-purple": "hsl(292, 42%, 14%)",
+    },
+    boxShadow: {
+      "main": "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+    },
+    extend: {
+      fontFamily: {
+        "work-sans": ["Work Sans", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
