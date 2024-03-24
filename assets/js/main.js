f11cce752ebd9cd454a9a566182470cd15551446
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-03-24 17:28:22 +0000

    Add js for the functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('FAQ Accordion');
+const questionWrappers = document.querySelectorAll('.question-wrapper');
+const allAnswers = document.querySelectorAll('.answer');
+const allIcons = document.querySelectorAll('.icon');
+
+questionWrappers.forEach(qWrapper => {
+    qWrapper.addEventListener('click', () => {
+        allAnswers.forEach(answer => {
+            answer.classList.remove('open');
+        });
+        allIcons.forEach(icon => {
+            icon.src = "./assets/images/icon-plus.svg";
+        });
+        qWrapper.querySelector('.icon').src = "./assets/images/icon-minus.svg";
+        qWrapper.nextElementSibling.classList.add('open');
+    });
+});
