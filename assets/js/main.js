83c908a77dd7d8ce37656ce1b2e71c34e70cc9d2
Aatmane Oulmouden <aatmaneoulmouden21@gmail.com>
2024-03-25 15:43:41 +0000

    Navigate the questions and hide/show answers using keyboard navigation alone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,30 @@
 const questionWrappers = document.querySelectorAll('.question-wrapper');
 const allAnswers = document.querySelectorAll('.answer');
 const allIcons = document.querySelectorAll('.icon');
+const questionLinks = document.querySelector('.question-link');
+
+// Show and hide answer function
+const showHideAnswer = (icon, qWrapper) => {
+    allAnswers.forEach(answer => {
+        answer.classList.remove('open');
+    });
+    allIcons.forEach(icon => {
+        icon.src = "./assets/images/icon-plus.svg";
+        icon.classList.remove('clicked');
+    });
+    icon.src = "./assets/images/icon-minus.svg";
+    icon.classList.add('clicked');
+    qWrapper.nextElementSibling.classList.add('open');
+}
 
 questionWrappers.forEach(qWrapper => {
     qWrapper.addEventListener('click', () => {
-        const icon = qWrapper.querySelector('.icon');
-        allAnswers.forEach(answer => {
-            answer.classList.remove('open');
-        });
-        allIcons.forEach(icon => {
-            icon.src = "./assets/images/icon-plus.svg";
-            icon.classList.remove('clicked');
-        });
-        icon.src = "./assets/images/icon-minus.svg";
-        icon.classList.add('clicked');
-        qWrapper.nextElementSibling.classList.add('open');
+        showHideAnswer(qWrapper.querySelector('.icon'), qWrapper);
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.key == qWrapper.dataset.key) {
+            showHideAnswer(qWrapper.querySelector('.icon'), qWrapper);
+        }
     });
 });
